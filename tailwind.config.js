e3fe29fdb5c97e352c327595c11c03c6e7113e89
Philip-P2205 <pipip2205@gmail.com>
2022-02-25 12:02:26 +0100

    cleaned up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,32 @@
 module.exports = {
-    // important: true,
-    mode: 'jit',
-    purge: {
-        content: [
-            './src/**/*.{ts,html,css}',
-        ]
-    },
-    darkMode: 'class', // or 'media' or 'false'
-    theme: {
-        extend: {
-            colors: {
-                text: {
-                    darker: '#d1d5db',
-                    dark: '#f3f4f6',
-                    light: '#111827',
-                    lighter: '#1f2937',
-                },
-                background: {
-                    darker: '#1f2937',
-                    dark: '#374151',
-                    light: '#d1d5db',
-                    lighter: '#f3f4f6',
-                }
-            },
-            zIndex: {
-                '-10': '-10',
-                '100': '100',
-            }
+  // important: true,
+  content: [
+    './src/**/*.{ts,html,css}',
+  ],
+  mode: 'jit',
+  darkMode: 'class', // or 'media' or 'false'
+  theme: {
+    extend: {
+      colors: {
+        text: {
+          darker: '#d1d5db',
+          dark: '#f3f4f6',
+          light: '#111827',
+          lighter: '#1f2937',
         },
+        background: {
+          darker: '#1f2937',
+          dark: '#374151',
+          light: '#d1d5db',
+          lighter: '#f3f4f6',
+        }
+      },
+      zIndex: {
+        '-10': '-10',
+        '100': '100',
+      }
+    },
 
-    },
-    variants: {
-        extend: {
-        },
-    },
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'),],
-};
+  },
+  plugins: [],
+}
